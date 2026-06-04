1baea7f28b3d68e9133edd8b83c8fcb624951074
Priya Kumari <kumaripriyaa18@gmail.com>
2026-06-05 00:42:28 +0530

    fix render port

--- server/server.js
+++ server/server.js
@@ -44,6 +44,8 @@ app.get("/", (req, res) => {
 
 // SERVER
 
-app.listen(5000, () => {
-  console.log("Server Started on Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Started on Port ${PORT}`);
 });
